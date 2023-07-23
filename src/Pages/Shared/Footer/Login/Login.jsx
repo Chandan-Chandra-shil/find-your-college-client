a2a41e1b2c177166a3428b18a49519f8ee10fbad
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-07-23 13:06:43 +0600

    add sing up and login fuctionality

--- src/Pages/Shared/Footer/Login/Login.jsx
+++ src/Pages/Shared/Footer/Login/Login.jsx
@@ -1,14 +1,49 @@
 import Lottie from "lottie-react";
 import loginImage from "../../../../assets/login.json";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../../../Provider/AuthProvider";
+import Swal from "sweetalert2";
 const Login = () => {
+  const { logIn } = useContext(AuthContext);
+ const location = useLocation();
+ const navigate = useNavigate();
+ const from = location.state?.from?.pathname || "/";
+// handle login 
+  
+  const handleLogIn = (event) => {
+    event.preventDefault();
+
+    const form = event.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    logIn(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        form.reset("");
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Login Successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
   return (
     <div className="md:mt-36 mt-0">
       <div className=" md:flex ">
         <div className=" md:w-1/2">
           <Lottie className="" animationData={loginImage} loop={true} />
         </div>
-        <form className="md:w-1/2 ">
+        <form onSubmit={handleLogIn} className="md:w-1/2 ">
           <div className="card-body border">
             <h1 className="text-5xl font-bold">Login now!</h1>
             <div className="form-control">
@@ -16,7 +51,8 @@ const Login = () => {
                 <span className="label-text">Email</span>
               </label>
               <input
-                type="text"
+                type="email"
+                name="email"
                 placeholder="email"
                 className="input input-bordered"
               />
@@ -26,7 +62,8 @@ const Login = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="text"
+                type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
               />
@@ -43,7 +80,6 @@ const Login = () => {
               <p>
                 Find Your College New?
                 <Link className="text-green-500" to="/singUp">
-                 
                   Sing Up
                 </Link>
               </p>
--- src/Pages/Shared/SignUp/SignUp.jsx
+++ src/Pages/Shared/SignUp/SignUp.jsx
@@ -1,23 +1,103 @@
 import Lottie from "lottie-react";
 import loginImage from "../../../assets/login.json";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../../Provider/AuthProvider";
+import { useContext } from "react";
+
+import Swal from "sweetalert2";
 const SignUp = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state?.from?.pathname || "/";
+  const {createUser} = useContext(AuthContext)
+
+  const handleSignUp = (event) => {
+    event.preventDefault()
+      const form = event.target;
+      const name = form.name.value;
+      const email = form.email.value;
+      const password = form.password.value;
+      const university = form.university.value;
+    const address = form.address.value;
+    const userInfo = {
+      name,
+      email,
+      password,
+      university,
+      address,
+    };
+
+    createUser(email, password)
+      .then((result) => {
+        const newUser = result.user;
+        form.reset();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: "Sign Up Successfully",
+          showConfirmButton: false,
+          timer: 1500,
+        });
+         navigate(from, { replace: true });
+
+        console.log(newUser);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+
+      console.log(userInfo);
+  }
   return (
-    <div className="md:mt-36 mt-0">
-      <div className=" md:flex ">
+    <div className="md:mt-12 mt-0">
+      <div className=" md:flex items-center ">
         <div className=" md:w-1/2">
           <Lottie className="" animationData={loginImage} loop={true} />
         </div>
-        <form className="md:w-1/2 ">
+        <form onSubmit={handleSignUp} className="md:w-1/2 ">
           <div className="card-body border">
             <h1 className="text-5xl font-bold">Sign Up now!</h1>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="name"
+                className="input input-bordered"
+              />
+            </div>
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
-                type="text"
+                type="email"
+                  name="email"
                 placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">University</span>
+              </label>
+              <input
+                type="text"
+                name="university"
+                placeholder="university"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Address</span>
+              </label>
+              <input
+                type="text"
+                name="address"
+                placeholder="address"
                 className="input input-bordered"
               />
             </div>
@@ -26,14 +106,15 @@ const SignUp = () => {
                 <span className="label-text">Password</span>
               </label>
               <input
-                type="text"
+                type="password"
+                name="password"
                 placeholder="password"
                 className="input input-bordered"
               />
               <label className="label">
-                <a href="#" className="label-text-alt link link-hover">
-                  Forgot password?
-                </a>
+                
+                 show error
+              
               </label>
             </div>
             <div className="form-control mt-6">
